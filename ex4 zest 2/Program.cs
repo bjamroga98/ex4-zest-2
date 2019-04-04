b72f147c78d26d96d4303dc050a72e73ae031efc
barbara.jamroga <barbara.jamroga@microsoft.wsei.edu.pl>
2019-04-04 09:16:56 +0200

    Commit

--- ex4 zest 2/Program.cs
+++ ex4 zest 2/Program.cs
@@ -10,6 +10,23 @@ namespace ex4_zest_2
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int a, b;
+                string text;
+                Console.Write("Enter the value of the dividend (a): ");
+                text = Console.ReadLine();
+                Int32.TryParse(text, out a);
+                Console.Write("Enter the value of the divisor (b): ");
+                text = Console.ReadLine();
+                Int32.TryParse(text, out b);
+                Console.WriteLine("Division result: {0}", a / b);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You shall not divide by 0!");
+            }
+            Console.ReadKey();
         }
     }
 }
